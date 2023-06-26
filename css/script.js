040eb5f0fd73edcc82a1a8c5bc80421cf4ce77ac
marcello415 <marcello_contact@hotmail.com>
2023-06-25 21:31:17 -0400

    first commit

---
+++ css/script.js
@@ -0,0 +1,11 @@
+function changeIframe(change) {
+  document.getElementById("webpage").src = change;
+}
+
+function toggle_visibility(id) {
+  var e = document.getElementById(id);
+  if(e.style.display == 'block')
+     e.style.display = 'none';
+  else
+     e.style.display = 'block';
+}
